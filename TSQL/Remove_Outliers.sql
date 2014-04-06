2823e5986b7286b7336dcdd28215c8a8e72bf8b8
Tim <tmmtsmith@gmail.com>
2014-04-06 04:37:49 -0500

    Remove outliers

---
+++ TSQL/Remove_Outliers.sql
@@ -0,0 +1,32 @@
+CREATE PROCEDURE stp_RemoveOutliers
+@t NVARCHAR(500), @v NVARCHAR(250), @dev DECIMAL(3,1)
+AS
+BEGIN
+
+	DECLARE @avg NVARCHAR(250), @stdev NVARCHAR(250), @id NVARCHAR(250)
+	SELECT @id = COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = @t AND COLUMN_NAME LIKE 'ID%'
+	SELECT @avg = COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = @t AND COLUMN_NAME LIKE '%Avg%'
+	SELECT @stdev = COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE  TABLE_NAME = @t AND COLUMN_NAME LIKE '%StDev%'
+
+	DECLARE @s NVARCHAR(MAX)
+	SET @s = ';WITH OutOutlier AS(
+		SELECT ' + @id + ' NewID
+			, ' + @v + ' OutValue
+			, (' + @avg + ' + (' + @stdev + ' *' + CAST(@dev AS NVARCHAR(3)) + ')) ThreeAbove
+			, (' + @avg + ' + (' + @stdev + ' *-' + CAST(@dev AS NVARCHAR(3)) + ')) ThreeBelow
+		FROM ' + @t + '
+	)
+	SELECT ROW_NUMBER() OVER (ORDER BY ' + @id + ') NoOutlierID
+		, t2.*
+	INTO ' + @t + '_NoOutliers
+	FROM OutOutlier t
+		INNER JOIN ' + @t + ' t2 ON t.NewID = t2.' + @id + '
+	WHERE t.OutValue BETWEEN ThreeBelow AND ThreeAbove
+	
+	ALTER TABLE ' + @t + '_NoOutliers DROP COLUMN ' + @id
+
+	EXEC sp_executesql @s
+	
+END
+
+EXECUTE stp_RemoveOutliers 'WaterTable','WaterMeasurement',1
