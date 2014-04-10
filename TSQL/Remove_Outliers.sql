63978f447a7c531b15f06cd66fcebd8297f4b42d
Tim <tmmtsmith@gmail.com>
2014-04-10 08:47:32 -0500

    Added schema adj

--- TSQL/Remove_Outliers.sql
+++ TSQL/Remove_Outliers.sql
@@ -7,7 +7,7 @@
 */
 
 CREATE PROCEDURE stp_RemoveOutliers
-@t NVARCHAR(100), @v NVARCHAR(100), @dev DECIMAL(3,1)
+@t NVARCHAR(100), @v NVARCHAR(100), @dev DECIMAL(3,1)--,@sh NVARCHAR(15)
 AS
 BEGIN
 
@@ -16,6 +16,7 @@ BEGIN
 	SELECT @avg = COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = @t AND COLUMN_NAME LIKE 'Avg%'
 	SELECT @stdev = COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE  TABLE_NAME = @t AND COLUMN_NAME LIKE 'StDev%'
 	SET @to = @t + '_NoOutliers'
+	-- SET @to = QUOTENAME(@sh) + '.' + QUOTENAME(@t + '_NoOutliers')
 
 	DECLARE @s NVARCHAR(MAX)
 	SET @s = N'IF OBJECT_ID(@to) IS NOT NULL
@@ -31,11 +32,13 @@ BEGIN
 		SELECT ROW_NUMBER() OVER (ORDER BY ' + @id + ') NoOutlierID
 			, t2.*
 		INTO ' + QUOTENAME(@t + '_NoOutliers') + '
+		-- INTO ' + QUOTENAME(@sh) + '.' + QUOTENAME(@t + '_NoOutliers') + '
 		FROM OutOutlier t
 			INNER JOIN ' + QUOTENAME(@t) + ' t2 ON t.NewID = t2.' + @id + '
 		WHERE t.OutValue BETWEEN OBelow AND OAbove
 		
 		ALTER TABLE ' + QUOTENAME(@t + '_NoOutliers') + ' DROP COLUMN ' + @id + '
+		-- ALTER TABLE ' + QUOTENAME(@sh) + '.' + QUOTENAME(@t + '_NoOutliers') + ' DROP COLUMN ' + @id + '
 	END'
 
 	EXEC sp_executesql @s,N'@to NVARCHAR(100)',@to
