f54f8f13bc3aab379ba6b050fa0cae754a00a938
Tim <tmmtsmith@gmail.com>
2014-04-10 09:18:53 -0500

    Added debugging

--- TSQL/Remove_Outliers.sql
+++ TSQL/Remove_Outliers.sql
@@ -7,7 +7,7 @@
 */
 
 CREATE PROCEDURE stp_RemoveOutliers
-@t NVARCHAR(100), @v NVARCHAR(100), @dev DECIMAL(3,1)--,@sh NVARCHAR(15)
+@t NVARCHAR(100), @v NVARCHAR(100), @dev DECIMAL(3,1), @sh NVARCHAR(15)
 AS
 BEGIN
 
@@ -15,34 +15,36 @@ BEGIN
 	SELECT @id = COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = @t AND COLUMN_NAME LIKE 'ID%'
 	SELECT @avg = COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = @t AND COLUMN_NAME LIKE 'Avg%'
 	SELECT @stdev = COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE  TABLE_NAME = @t AND COLUMN_NAME LIKE 'StDev%'
-	SET @to = @t + '_NoOutliers'
-	-- SET @to = QUOTENAME(@sh) + '.' + QUOTENAME(@t + '_NoOutliers')
+	--SET @to = @t + '_NoOutliers'
+	SET @to = QUOTENAME(@sh) + '.' + QUOTENAME(@t + '_NoOutliers')
+
 
 	DECLARE @s NVARCHAR(MAX)
-	SET @s = N'IF OBJECT_ID(@to) IS NOT NULL
+	SET @s = N'IF OBJECT_ID(@to) IS NULL
 	BEGIN
 	
 		;WITH OutOutlier AS(
-			SELECT ' + @id + ' NewID
+			SELECT ' + @id + ' NewestID
 				, ' + @v + ' OutValue
 				, (' + @avg + ' + (' + @stdev + ' *' + CAST(@dev AS NVARCHAR(3)) + ')) OAbove
 				, (' + @avg + ' + (' + @stdev + ' *-' + CAST(@dev AS NVARCHAR(3)) + ')) OBelow
-			FROM ' + QUOTENAME(@t) + '
+			FROM ' + QUOTENAME(@sh) + '.' + QUOTENAME(@t) + '
 		)
 		SELECT ROW_NUMBER() OVER (ORDER BY ' + @id + ') NoOutlierID
 			, t2.*
-		INTO ' + QUOTENAME(@t + '_NoOutliers') + '
-		-- INTO ' + QUOTENAME(@sh) + '.' + QUOTENAME(@t + '_NoOutliers') + '
+		--INTO ' + QUOTENAME(@t + '_NoOutliers') + '
+		INTO ' + QUOTENAME(@sh) + '.' + QUOTENAME(@t + '_NoOutliers') + '
 		FROM OutOutlier t
-			INNER JOIN ' + QUOTENAME(@t) + ' t2 ON t.NewID = t2.' + @id + '
+			INNER JOIN ' + QUOTENAME(@sh) + '.' + QUOTENAME(@t) + ' t2 ON t.NewestID = t2.' + @id + '
 		WHERE t.OutValue BETWEEN OBelow AND OAbove
 		
-		ALTER TABLE ' + QUOTENAME(@t + '_NoOutliers') + ' DROP COLUMN ' + @id + '
-		-- ALTER TABLE ' + QUOTENAME(@sh) + '.' + QUOTENAME(@t + '_NoOutliers') + ' DROP COLUMN ' + @id + '
+		--ALTER TABLE ' + QUOTENAME(@t + '_NoOutliers') + ' DROP COLUMN ' + @id + '
+		ALTER TABLE ' + QUOTENAME(@sh) + '.' + QUOTENAME(@t + '_NoOutliers') + ' DROP COLUMN ' + @id + '
 	END'
 
+	--PRINT @s
 	EXEC sp_executesql @s,N'@to NVARCHAR(100)',@to
-	
+
 END
 
-EXECUTE stp_RemoveOutliers 'WaterTable','WaterMeasurement',1
+EXECUTE stp_RemoveOutliers 'WaterTable','WaterMeasurement',1,'wtr'
